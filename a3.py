f49efa6af689ff047d5b14bea5fae428ddc7a8bc
Youssef Ahmed Yahia <yahmedyahia1@cps.edu>
2025-10-02 11:04:02 -0500

    searchlist

--- a3.py
+++ a3.py
@@ -249,7 +249,13 @@ def search_pa_list(src: List[str]) -> List[str]:
         a list of answers. Will be ["I don't understand"] if it finds no matches and
         ["No answers"] if it finds a match but no answers
     """
-    pass
+    for pat, act in pa_list:
+        mat = match(pat, src)
+
+        if mat is not None:
+            answer = act(mat)
+            return answer if answer else ["No answers"]
+    return ["I don't understand"]
 
 
 def query_loop() -> None:
